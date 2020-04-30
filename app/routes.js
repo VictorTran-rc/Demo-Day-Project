9780bc16c061169bd6c00f307f9c5d4d77a27d75
Victor Tran <vicphitran@gmail.com>
2020-04-30 14:54:19 -0400

    signup page takes you to home

--- app/routes.js
+++ app/routes.js
@@ -329,7 +329,7 @@ module.exports = function(app, passport, db, nexmo, objectId) {
 
   // process the signup form
   app.post('/signup', passport.authenticate('local-signup', {
-    successRedirect: '/profile', // redirect to the secure profile section
+    successRedirect: '/index', // redirect to the secure profile section
     failureRedirect: '/signup', // redirect back to the signup page if there is an error
     failureFlash: true // allow flash messages
   }));
